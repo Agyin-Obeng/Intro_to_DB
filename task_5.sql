b660b82bc48994587f4b485db236cb5fc2efa92c
Agyin-Obeng <agyinobeng60@gmail.com>
2025-10-30 20:12:37 +0000

    Fix SQL commands and table name in task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,9 +1,9 @@
--- Use the database
+-- Select the database
 USE alx_book_store;
 
--- Insert a single record into the Customers table
-INSERT INTO Customers (customer_id, customer_name, email, address)
+-- Insert a single record into the Customer table
+INSERT INTO Customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
 
 How To Run
-mysql -u root -p < task_5.sql
+mysql -u root -p alx_book_store < task_5.sql
