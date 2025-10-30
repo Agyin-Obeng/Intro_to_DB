f50bb1c2e0dedd944eae488a242920eb0fb80835
Agyin-Obeng <agyinobeng60@gmail.com>
2025-10-30 19:40:20 +0000

    Create task_3.sql to show all database tables
    Adds SQL script to list all tables in a database.

---
+++ task_3.sql
@@ -0,0 +1,9 @@
+-- List all tables in the database passed as argument
+SHOW TABLES;
+
+
+How To Run
+mysql -u root -p alx_book_store < task_3.sql
+
+OR
+mysql -u root -p your_database_name < task_3.sql
